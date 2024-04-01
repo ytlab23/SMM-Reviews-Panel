e7bdcda9940c02554280e21cc06bc9aa6a22ef9f
Bishal Biswas <bbiswas47@outlook.com>
2024-04-02 01:01:26 +0530

    Vercel Deploy URL Added

--- astro.config.mjs
+++ astro.config.mjs
@@ -5,7 +5,7 @@ import vercel from "@astrojs/vercel/serverless";
 
 // https://astro.build/config
 export default defineConfig({
-  site: 'http://localhost:4321/',
+  site: 'https://smm-reviews-panel.vercel.app/',
   output: 'server',
   integrations: [tailwind()],
   adapter: vercel()
