aca9025d3bb13331574763595208ff8a4474c41d
Bishal Biswas <bbiswas47@outlook.com>
2024-05-09 00:44:37 +0530

    Sitemap, Search and Responsive

--- astro.config.mjs
+++ astro.config.mjs
@@ -1,13 +1,14 @@
 import { defineConfig } from 'astro/config';
 import tailwind from "@astrojs/tailwind";
-
 import vercel from "@astrojs/vercel/serverless";
+
+import sitemap from "@astrojs/sitemap";
 
 // https://astro.build/config
 export default defineConfig({
   // site: 'http://localhost:4321',
   site: 'https://smm-reviews-panel.vercel.app',
   output: 'server',
-  integrations: [tailwind()],
+  integrations: [tailwind(), sitemap()],
   adapter: vercel()
 });
